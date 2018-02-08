6bfb7d6cad2be3cfe5d06f8c8db45db8756a8ff6
Bruno Gomes <brunogomess7@gmail.com>
2018-02-08 14:17:53 -0300

    Menu interativo e outras páginas

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,4 @@
+
+function mudaFoto(foto){
+			document.getElementById("icone").src = "_imagens/"+foto+".png"; /* Trocará a imagem atual pela a que for mandada como parâmetro nas funções 'onmouseover' e 'onmouseout' presentes lá na lista. */
+}
